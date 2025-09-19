21e58ec795175a7d6edf6fc952958542f522e045
lucas chen <lucas.chen10@outlook.com>
2025-09-19 13:37:49 +1000

    restored

---
+++ .history/database_manager_20250919133551.py
@@ -0,0 +1,15 @@
+import sqlite3 as sql
+import os
+
+
+class DBManager:
+    def __init__(self):
+        self.dbPath = os.path.join(os.getcwd(), "database", "data_source.db")
+
+
+def listExtension():
+    con = sql.connect("database/data_source.db")
+    cur = gitcon.cursor()
+    data = cur.execute("SELECT * FROM extension").fetchall()
+    con.close()
+    return data
